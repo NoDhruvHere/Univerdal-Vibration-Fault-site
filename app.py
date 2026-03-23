8df0f39f95c27e639b37a3b98121e0cdb37ee358
NoDhruvHere <hydradhruv99@gmail.com>
2026-03-23 22:44:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import tensorflow as tf
+import tflite_runtime.interpreter as tftensorflow as tfimport
 from tensorflow.keras.models import load_model
 import joblib
 import plotly.graph_objects as go
@@ -11,6 +11,19 @@ import os
 import glob
 from datetime import datetime
 
+@st.cache_resource
+def load_tflite_model():
+    interpreter = tflite.Interpreter(model_path='results/cnn_model.tflite')
+    interpreter.allocate_tensors()
+    return interpreter
+
+def predict_with_tflite(interpreter, input_data):
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+    
+    interpreter.set_tensor(input_details[0]['index'], input_data.astype(np.float32))
+    interpreter.invoke()
+    return interpreter.get_tensor(output_details[0]['index'])[0]
 # Page configuration
 st.set_page_config(
     page_title="Gearbox Fault Detection System",
